29805d30e98150d2ce3a3ce472c012df2413f5c1
PAClFlER <131374307+PAClFlER@users.noreply.github.com>
2023-07-13 10:45:45 +0300

    Update data_store.py

--- data_store.py
+++ data_store.py
@@ -4,12 +4,16 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import Session
 from config import db_url_object
+
 metadata = MetaData()
 Base = declarative_base()
+
+
 class Viewed(Base):
     __tablename__ = 'viewed'
     profile_id = sq.Column(sq.Integer, primary_key=True)
     worksheet_id = sq.Column(sq.Integer, primary_key=True)
+
 
 # добавление записи в бд
 def add_user(engine, profile_id, worksheet_id):
@@ -17,6 +21,7 @@ def add_user(engine, profile_id, worksheet_id):
         to_bd = Viewed(profile_id=profile_id, worksheet_id=worksheet_id)
         session.add(to_bd)
         session.commit()
+
 
 # извлечение записей из БД
 def check_user(engine, profile_id, worksheet_id):
@@ -26,6 +31,8 @@ def check_user(engine, profile_id, worksheet_id):
             Viewed.worksheet_id == worksheet_id
         ).first()
         return True if from_bd else False
+
+
 if __name__ == '__main__':
     engine = sq.create_engine(db_url_object)
     Base.metadata.create_all(engine)
